12b7869214a8e2da8d2f739651faf4ae4899a296
MWarCZ <miroslav@tenvalka.eu>
2021-04-18 18:06:03 +0200

    Add component Avatar and picture with me

---
+++ src/components/Avatar.vue
@@ -0,0 +1,27 @@
+<template>
+  <img
+    :src="src"
+    :alt="alt"
+    :class="`avatar ${($props.class)?$props.class:''}`"
+    :style="(size)? `--size: ${size};`: ''"
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    src: { type: String, required: true },
+    alt: { type: String, required: true },
+    size: { type: String },
+  },
+}
+</script>
+
+<style scoped>
+.avatar {
+  width: var(--size, 2em);
+  height: var(--size, 2em);
+  object-fit: cover;
+  border-radius: 50%;
+}
+</style>
